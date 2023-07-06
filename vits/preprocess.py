22a11386ba2ebfefb1e83a445332992745e7bc71
Roista57 <91594792+Roista57@users.noreply.github.com>
2023-07-06 09:39:40 +0900

    Edit preprocess.py

--- vits/preprocess.py
+++ vits/preprocess.py
@@ -1,6 +1,7 @@
 import argparse
 import text
 from utils import load_filepaths_and_text
+from tqdm import tqdm
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -13,16 +14,16 @@ if __name__ == '__main__':
     
 
   for filelist in args.filelists:
-    print("START:", filelist)
+    #print("START:", filelist)
     filepaths_and_text = load_filepaths_and_text(filelist)
-    for i in range(len(filepaths_and_text)):
+    for i in tqdm(range(len(filepaths_and_text)), desc="Vits preprocess :"):
       original_text = filepaths_and_text[i][args.text_index]
       cleaned_text = text._clean_text(original_text, args.text_cleaners)
       filepaths_and_text[i][args.text_index] = cleaned_text
       # Print the cleaned text
-      print("Original Text:", original_text)
-      print("Cleaned Text:", cleaned_text)
-      print("\n")
+      #print("Original Text:", original_text)
+      #print("Cleaned Text:", cleaned_text)
+      #print("\n")
 
     new_filelist = filelist + "." + args.out_extension
     with open(new_filelist, "w", encoding="utf-8") as f:
